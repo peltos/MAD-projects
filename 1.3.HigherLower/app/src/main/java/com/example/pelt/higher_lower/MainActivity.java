f1a5ce0a54ee0283f851fe66b1bda4d48f5b0825
Ron Pelt <pelt8@hotmail.com>
2018-09-13 13:13:06 +0200

    higher lower fix

--- 1.3.HigherLower/app/src/main/java/com/example/pelt/higher_lower/MainActivity.java
+++ 1.3.HigherLower/app/src/main/java/com/example/pelt/higher_lower/MainActivity.java
@@ -124,20 +124,6 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     private void updateUI() {
         if (mAdapter == null) {
