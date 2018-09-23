943e501579b2311038eafc084797fe7580a3fc91
Ron Pelt <pelt8@hotmail.com>
2018-09-23 17:23:55 +0200

    changed all examples into 1 example

---
+++ example/app/src/main/java/com/example/pelt/example/MainActivity.java
@@ -0,0 +1,161 @@
+package com.example.pelt.example;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements ReminderAdapter.ReminderClickListener {
+
+    //Local variables
+
+    private List<Reminder> mReminders;
+    private ReminderAdapter mAdapter;
+    private RecyclerView mRecyclerView;
+    private EditText mNewReminderText;
+
+    //Constants used when calling the update activity
+    public static final String EXTRA_REMINDER = "Reminder";
+    public static final int REQUESTCODE = 1234;
+    private int mModifyPosition;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initialize the local variables
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        mNewReminderText = findViewById(R.id.editText_main);
+
+        mReminders = new ArrayList<>();
+        mRecyclerView.setAdapter(mAdapter);
+
+        // Create a grid where items can position horizontally, its now set on 2 columns
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+
+        updateUI();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Get the user text from the textfield
+                String text = mNewReminderText.getText().toString();
+                Reminder newReminder = new Reminder(text);
+
+                //Check if some text has been added
+                if (!(TextUtils.isEmpty(text))) {
+                    //Add the text to the list (datamodel)
+                    mReminders.add(newReminder);
+
+                    //Tell the adapter that the data set has been modified: the screen will be refreshed.
+                    mAdapter.notifyDataSetChanged();
+
+                    //Initialize the EditText for the next item
+                    mNewReminderText.setText("");
+                } else {
+                    //Show a message to the user if the textfield is empty
+                    Snackbar.make(view, "Please enter some text in the textfield", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                }
+            }
+        });
+
+        /* Add a touch helper to the RecyclerView to recognize when a user swipes to delete a list entry.
+        An ItemTouchHelper enables touch behavior (like swipe and move) on each ViewHolder,
+        and uses callbacks to signal when a user is performing these actions. */
+
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+            //Called when a user swipes left or right on a ViewHolder
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                //Get the index corresponding to the selected position
+                int position = (viewHolder.getAdapterPosition());
+                mReminders.remove(position);
+                mAdapter.notifyItemRemoved(position);
+            }
+        };
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        itemTouchHelper.attachToRecyclerView(mRecyclerView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void updateUI() {
+
+        if (mAdapter == null) {
+            mAdapter = new ReminderAdapter( mReminders, this);
+            mRecyclerView.setAdapter(mAdapter);
+        } else {
+            mAdapter.notifyDataSetChanged();
+        }
+
+    }
+
+    @Override
+    public void reminderOnClick(int i) {
+        Toast.makeText(this, "Test "+i, Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(MainActivity.this, UpdateActivity.class);
+        mModifyPosition = i;
+        intent.putExtra(EXTRA_REMINDER, mReminders.get(i));
+        startActivityForResult(intent, REQUESTCODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUESTCODE) {
+            if (resultCode == RESULT_OK) {
+                Reminder updatedReminder = data.getParcelableExtra(MainActivity.EXTRA_REMINDER);
+                // New timestamp: timestamp of update
+                mReminders.set(mModifyPosition, updatedReminder);
+                updateUI();
+            }
+        }
+    }
+}
---
+++ example/app/src/main/java/com/example/pelt/example/ReminderAdapter.java
@@ -0,0 +1,64 @@
+package com.example.pelt.example;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ReminderAdapter extends RecyclerView.Adapter<ReminderAdapter.ViewHolder> {
+
+    private List<Reminder> mReminders;
+
+    public ReminderAdapter(List<Reminder> reminders, ReminderClickListener mReminderClickListener) {
+        mReminders = reminders;
+        this.mReminderClickListener = mReminderClickListener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater= LayoutInflater.from(context);
+        View view = inflater.inflate(android.R.layout.simple_list_item_1, null);
+
+        // Return a new holder instance
+        ReminderAdapter.ViewHolder viewHolder = new ReminderAdapter.ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Reminder reminder =  mReminders.get(position);
+        holder.textView.setText(reminder.getmReminderText());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mReminders.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        public TextView textView;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            textView=  itemView.findViewById(android.R.id.text1);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int clickedPosition = getAdapterPosition();
+            mReminderClickListener.reminderOnClick(clickedPosition);
+        }
+    }
+
+    final private ReminderClickListener mReminderClickListener;
+
+    public interface ReminderClickListener{
+        void reminderOnClick (int i);
+    }
+}
+
