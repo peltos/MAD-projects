d32bae6984cd4c942401ef068c83952515d17b9b
Ron Pelt <pelt8@hotmail.com>
2019-01-05 12:48:33 +0100

    added example level 6

---
+++ example6/app/src/main/java/com/example/pelt/example6/Animal.java
@@ -0,0 +1,21 @@
+package com.example.pelt.example6;
+
+public class Animal {
+
+    private int id;
+    private int imageRes;
+
+    public Animal(int id, int imageRes) {
+        this.id = id;
+        this.imageRes = imageRes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getImageRes() {
+        return imageRes;
+    }
+
+}
